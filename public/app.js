2ff99e38e17b6a0649d934142fa3da0f851c4726
MoNsTeR <zgm_striker17@hotmail.com>
2018-09-21 15:01:27 -0400

    Not solved

--- public/app.js
+++ public/app.js
@@ -1,5 +1,23 @@
-console.log('something');
 
-$.getJSON('/api/list').then(function(data){
-    console.log(data);
-})
+const addListItem = function(event){
+    event.preventDefault();
+    console.log('Anthing');
+    $.getJSON('/api/list').then(data => {
+        console.log(data);
+    const variable = data.map(element => `<li>${element}</li>`)
+    $('#todo-list').html(variable);
+    console.log(variable);
+    
+});
+}
+
+const submitItem = function(event){
+    event.preventDefault();
+    const data = $('input').val().trim();
+    $.ajax({url: "/add", method: "POST", data: data});
+}
+
+$('form').on('submit', submitItem);
+$('button').on('click', addListItem);
+
+
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,15 @@
-const list = [];
+const list = require('../Data/todolist');
 
 module.exports = function(app){
-    app.get('/', function(req, res){
-        res.send('Hey whats up?');
-    })
+    
     app.post('/add', function(req, res){
        console.log(req.body);
-       list.push(req.body.Todo);
-       res.redirect('/');
+       list.push(req.body);
+       res.send(list);
+    //    res.redirect('/');
+    })
+    app.get('/', function(req, res){
+        res.json(list);
     })
     app.get('/api/list', function(req, res){
         res.json(list);

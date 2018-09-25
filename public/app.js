08790b3eff7db36d6dc98df61ab695cc5bea5414
MoNsTeR <zgm_striker17@hotmail.com>
2018-09-25 18:31:51 -0400

    Added styling

--- public/app.js
+++ public/app.js
@@ -2,9 +2,7 @@ const deleteItem = function (event) {
     event.preventDefault();
     const index = event.target.id;
     $.ajax({ url: "/delete", method: "DELETE", data: { index: index } }).then(listItem => {
-        const variable = listItem.map((e, index) => `<input type="checkbox" class="checkbox"></input>
-        <li>${e}</li>
-        <i class="fas fa-times" id='${index}'></i><br>`);
+        const variable = listItem.map((e, index) => `<li><input type="checkbox" class="checkbox"></input>${e}<i class="fas fa-times" id='${index}'></i></li><br>`);
         $('#todo-list').html(variable);
         $('.fa-times').on('click', deleteItem);
     });
@@ -14,9 +12,7 @@ const submitItem = function (event) {
     event.preventDefault();
     const data = $('input').val().trim();
     $.ajax({ url: "/add", method: "POST", data: { TodoItem: data } }).then(data => {
-        const variable = data.map((element, index) => `<input type="checkbox" class="checkbox"></input>
-        <li>${element}</li>
-        <i class="fas fa-times" id='${index}'></i><br>`)
+        const variable = data.map((element, index) => `<li><input type="checkbox" class="checkbox"></input>${element}<i class="fas fa-times" id='${index}'></i></li><br>`)
         $('#todo-list').html(variable);
         $('.fa-times').on('click', deleteItem);
     });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,16 +3,16 @@ const list = [];
     app.post('/add', function(req, res){
        list.push(req.body.TodoItem);
        res.send(list);
-    })
-    app.get('/', function(req, res){
-        res.json(list);
-    })
+    });
     app.get('/api/list', function(req, res){
         res.json(list);
-    })
+    });
     app.delete('/delete', function(req, res){
          list.splice(req.body.index, 1);
          res.json(list);
          console.log(list);
-    })
+    });
+    // app.put('/update', function(req, res){
+        
+    // })
 }

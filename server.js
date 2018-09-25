c84bb390dd3ee16734a561d48e6b0962f2351b0d
zmiles17 <zgm_striker17@hotmail.com>
2018-09-24 22:34:28 -0400

    Changed to PORT 4000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
  const app = express();
- const PORT = 8080;
+ const PORT = process.env.PORT || 4000;
  app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
  app.use(express.static(path.join(__dirname, 'public')));

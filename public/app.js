56a55dc395ac794f3484ebc996121df51e888c49
zmiles17 <zgm_striker17@hotmail.com>
2018-09-27 14:36:38 -0400

    Centered UL

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const deleteItem = function (event) {
     event.preventDefault();
     const index = event.target.id;
-    $.ajax({ url: "/delete", method: "DELETE", data: { index: index } }).then(listItem => {
+    $.ajax({ url: "/delete", method: "DELETE", data: { index: index } }).then(function() {
         getItems();
     });
 }
@@ -9,23 +9,22 @@ const deleteItem = function (event) {
 const submitItem = function (event) {
     event.preventDefault();
     const data = $('input').val().trim();
-    $.ajax({ url: "/add", method: "POST", data: { TodoItem: data } }).then(data => {
+    $.ajax({ url: "/add", method: "POST", data: { TodoItem: data } }).then(function() {
         getItems();
     });
 }
 
 const getItems = function () {
     $.getJSON('/api/list').then(function (data) {
-        const variable = data.map((element, index) => `<input type="checkbox" class="checkbox" ${element.completed ? 'checked' : ''} data-id=${index}></input>
-        <li>${element.name}</li>
-        <i class="fas fa-times" id='${index}'></i><br>`)
+        const variable = data.map((element, index) => `<input type="checkbox" class="checkbox" ${element.completed ? 'checked' : ''} data-id=${index}></input><li>${element.name}</li><i class="fas fa-times" id='${index}'></i><br>`)
         $('#todo-list').html(variable);
         $('.fa-times').on('click', deleteItem);
         $('.checkbox').on('click', function (event) {
             const ID = $(event.target).attr('data-id');
-            $.ajax({ url: '/api/update', method: 'PUT', data: { ID: ID } }).then(function (res) {
-                getItems();
-            })
+            $.ajax({ url: '/api/update', method: 'PUT', data: { ID: ID } })
+                .then(function (res) {
+                    getItems();
+                })
                 .catch(function (err) {
                     alert(err);
                 })
